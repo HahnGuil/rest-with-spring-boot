132e81f15583ec73885e7b2ced078e2420a55cc8
HahnGuil <guilherme.f.h@hotmail.com>
2023-10-04 11:12:16 -0300

    Update calculadora

---
+++ demo/src/main/java/br/com/hahn/controllers/exceptions/UnsupportedMathOperationException.java
@@ -0,0 +1,17 @@
+package br.com.hahn.controllers.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException {
+
+    public UnsupportedMathOperationException(String ex){
+        super(ex);
+    }
+
+    @Serial
+    private static final long   serialVersionUID = 1L;
+}
---
+++ demo/src/main/java/br/com/hahn/controllers/exceptions/handler/CostomizedResponseEntityHandler.java
@@ -0,0 +1,31 @@
+package br.com.hahn.controllers.exceptions.handler;
+
+import br.com.hahn.controllers.exceptions.ExceptionResponse;
+import br.com.hahn.controllers.exceptions.UnsupportedMathOperationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+@RestController
+public class CostomizedResponseEntityHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsupportedMathOperationException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
